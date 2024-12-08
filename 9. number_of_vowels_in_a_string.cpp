77d0e68a49af53e0bef4fabdc3af479fcb25a55a
Dip <65559847+Dip-Barua@users.noreply.github.com>
2024-12-08 23:41:09 +0600

    Added: C++ Program to count the number of vowels in a string

---
+++ 9. number_of_vowels_in_a_string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>  
+using namespace std;
+
+int main() {
+    string input;
+    int vowelCount = 0;
+    
+    cout << "Enter a string: ";
+    getline(cin, input);
+    
+    for(char c : input) {
+        c = tolower(c);
+        
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            vowelCount++; 
+    }
+
+    cout << "Number of vowels in \"" << input << "\" is: " << vowelCount << endl;
+    
+    return 0;
+}
